414781ae0236b14921ab25b8454a108c764dedf0
raoulvdberge <raoulvdberge@gmail.com>
2020-10-29 21:56:56 +0100

    Add some basic items.

--- fabric/src/main/java/com/refinedmods/refinedstorage2/fabric/init/RefinedStorage2Items.java
+++ fabric/src/main/java/com/refinedmods/refinedstorage2/fabric/init/RefinedStorage2Items.java
@@ -1,16 +1,25 @@
 package com.refinedmods.refinedstorage2.fabric.init;
 
+import com.refinedmods.refinedstorage2.fabric.item.ProcessorBindingItem;
+import com.refinedmods.refinedstorage2.fabric.item.ProcessorItem;
 import com.refinedmods.refinedstorage2.fabric.item.QuartzEnrichedIronItem;
+import com.refinedmods.refinedstorage2.fabric.item.SiliconItem;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RefinedStorage2Items {
     private BlockItem cable;
     private QuartzEnrichedIronItem quartzEnrichedIron;
     private BlockItem quartzEnrichedIronBlock;
+    private SiliconItem silicon;
+    private ProcessorBindingItem processorBinding;
+    private Map<ProcessorItem.Type, ProcessorItem> processors = new HashMap<>();
 
     public void register(String namespace, RefinedStorage2Blocks blocks, ItemGroup itemGroup) {
         cable = new BlockItem(blocks.getCable(), new Item.Settings().group(itemGroup));
@@ -19,5 +28,12 @@ public class RefinedStorage2Items {
         quartzEnrichedIron = Registry.register(Registry.ITEM, new Identifier(namespace, "quartz_enriched_iron"), new QuartzEnrichedIronItem(new Item.Settings().group(itemGroup)));
 
         Registry.register(Registry.ITEM, new Identifier(namespace, "quartz_enriched_iron_block"), new BlockItem(blocks.getQuartzEnrichedIron(), new Item.Settings().group(itemGroup)));
+
+        for (ProcessorItem.Type type : ProcessorItem.Type.values()) {
+            processors.put(type, Registry.register(Registry.ITEM, new Identifier(namespace, type.getName() + "_processor"), new ProcessorItem(new Item.Settings().group(itemGroup))));
+        }
+
+        silicon = Registry.register(Registry.ITEM, new Identifier(namespace, "silicon"), new SiliconItem(new Item.Settings().group(itemGroup)));
+        processorBinding = Registry.register(Registry.ITEM, new Identifier(namespace, "processor_binding"), new ProcessorBindingItem(new Item.Settings().group(itemGroup)));
     }
 }
---
+++ fabric/src/main/java/com/refinedmods/refinedstorage2/fabric/item/SiliconItem.java
@@ -0,0 +1,9 @@
+package com.refinedmods.refinedstorage2.fabric.item;
+
+import net.minecraft.item.Item;
+
+public class SiliconItem extends Item {
+    public SiliconItem(Settings settings) {
+        super(settings);
+    }
+}
