d782ebecc3bff4d6a102775b981ff255beb2d35e
raoulvdberge <raoulvdberge@gmail.com>
2020-11-28 20:34:29 +0100

    Add scrollbar

--- fabric/src/main/java/com/refinedmods/refinedstorage2/fabric/screen/GridScreen.java
+++ fabric/src/main/java/com/refinedmods/refinedstorage2/fabric/screen/GridScreen.java
@@ -3,6 +3,7 @@ package com.refinedmods.refinedstorage2.fabric.screen;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.refinedmods.refinedstorage2.fabric.RefinedStorage2Mod;
 import com.refinedmods.refinedstorage2.fabric.screen.handler.GridScreenHandler;
+import com.refinedmods.refinedstorage2.fabric.screen.widget.ScrollbarWidget;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
@@ -15,23 +16,31 @@ public class GridScreen extends HandledScreen<GridScreenHandler> {
     private static final int TOP_HEIGHT = 19;
     private static final int BOTTOM_HEIGHT = 99;
 
+    private ScrollbarWidget scrollbar;
+
     public GridScreen(GridScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
 
-        this.titleX = 7;
-        this.titleY = 7;
-        this.playerInventoryTitleX = 7;
-        this.playerInventoryTitleY = 75;
-        this.backgroundWidth = 227;
-        this.backgroundHeight = 176;
+        titleX = 7;
+        titleY = 7;
+        playerInventoryTitleX = 7;
+        playerInventoryTitleY = 75;
+        backgroundWidth = 227;
+        backgroundHeight = 176;
     }
 
     @Override
     protected void init() {
-        this.backgroundHeight = TOP_HEIGHT + (getVisibleRows() * 18) + BOTTOM_HEIGHT;
-        this.playerInventoryTitleY = backgroundHeight - BOTTOM_HEIGHT + 4;
+        backgroundHeight = TOP_HEIGHT + (getVisibleRows() * 18) + BOTTOM_HEIGHT;
+        playerInventoryTitleY = backgroundHeight - BOTTOM_HEIGHT + 4;
+
         super.init();
+
         getScreenHandler().addSlots(backgroundHeight - BOTTOM_HEIGHT + 17);
+
+        scrollbar = new ScrollbarWidget(client, x + 174, y + 20, 12, (getVisibleRows() * 18) - 2);
+
+        children.add(scrollbar);
     }
 
     private int getVisibleRows() {
@@ -67,13 +76,39 @@ public class GridScreen extends HandledScreen<GridScreenHandler> {
     @Override
     protected void drawForeground(MatrixStack matrices, int mouseX, int mouseY) {
         super.drawForeground(matrices, mouseX, mouseY);
-
     }
 
     @Override
-    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float partialTicks) {
         renderBackground(matrices);
-        super.render(matrices, mouseX, mouseY, delta);
+        super.render(matrices, mouseX, mouseY, partialTicks);
         drawMouseoverTooltip(matrices, mouseX, mouseY);
+        scrollbar.render(matrices, mouseX, mouseY, partialTicks);
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int clickedButton) {
+        if (scrollbar.mouseClicked(mouseX, mouseY, clickedButton)) {
+            return true;
+        }
+
+        return super.mouseClicked(mouseX, mouseY, clickedButton);
+    }
+
+    @Override
+    public void mouseMoved(double mx, double my) {
+        scrollbar.mouseMoved(mx, my);
+
+        super.mouseMoved(mx, my);
+    }
+
+    @Override
+    public boolean mouseReleased(double mx, double my, int button) {
+        return scrollbar.mouseReleased(mx, my, button) || super.mouseReleased(mx, my, button);
+    }
+
+    @Override
+    public boolean mouseScrolled(double x, double y, double delta) {
+        return this.scrollbar.mouseScrolled(x, y, delta) || super.mouseScrolled(x, y, delta);
     }
 }
---
+++ fabric/src/main/java/com/refinedmods/refinedstorage2/fabric/screen/widget/ScrollbarWidget.java
@@ -0,0 +1,95 @@
+package com.refinedmods.refinedstorage2.fabric.screen.widget;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.refinedmods.refinedstorage2.fabric.RefinedStorage2Mod;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.Drawable;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.gui.Element;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
+
+public class ScrollbarWidget extends DrawableHelper implements Element, Drawable {
+    private static final Identifier TEXTURE = new Identifier(RefinedStorage2Mod.ID, "textures/gui/widgets.png");
+    private static final int SCROLLER_HEIGHT = 15;
+
+    private final MinecraftClient client;
+    private final int x;
+    private final int y;
+    private final int width;
+    private final int height;
+
+    private int offset;
+    private int maxOffset;
+    private boolean enabled = true;
+    private boolean clicked;
+
+    public ScrollbarWidget(MinecraftClient client, int x, int y, int width, int height) {
+        this.client = client;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        maxOffset = 10;
+    }
+
+    @Override
+    public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+        RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+        client.getTextureManager().bindTexture(TEXTURE);
+        drawTexture(matrixStack, x, y + (int) ((float) offset / (float) maxOffset * (float) (height - SCROLLER_HEIGHT)), enabled ? 232 : 244, 0, 12, 15);
+    }
+
+    @Override
+    public void mouseMoved(double mouseX, double mouseY) {
+        if (clicked && mouseX >= x && mouseY >= y && mouseX <= x + width && mouseY <= y + height) {
+            updateOffset(mouseY);
+        }
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        if (button == 0 && mouseX >= x && mouseY >= y && mouseX <= x + width && mouseY <= y + height) {
+            updateOffset(mouseY);
+            clicked = true;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean mouseReleased(double mouseX, double mouseY, int button) {
+        if (clicked) {
+            clicked = false;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double scrollDelta) {
+        if (enabled) {
+            setOffset(offset + Math.max(Math.min(-(int) scrollDelta, 1), -1));
+            return true;
+        }
+        return false;
+    }
+
+    public void setMaxOffset(int maxOffset) {
+        this.maxOffset = maxOffset;
+
+        if (offset > maxOffset) {
+            this.offset = Math.max(0, maxOffset);
+        }
+    }
+
+    public void setOffset(int offset) {
+        if (offset >= 0 && offset <= maxOffset) {
+            this.offset = offset;
+        }
+    }
+
+    private void updateOffset(double mouseY) {
+        setOffset((int) Math.floor((float) (mouseY - y) / (float) (height - SCROLLER_HEIGHT) * (float) maxOffset));
+    }
+}
