f9534a1e84d288b8489ec3422d44b5fc39a5b559
raoulvdberge <raoulvdberge@gmail.com>
2020-11-29 15:03:52 +0100

    Finish ItemStorageChannel tests

--- core/src/test/java/com/refinedmods/refinedstorage2/core/storage/ItemStorageChannelTest.java
+++ core/src/test/java/com/refinedmods/refinedstorage2/core/storage/ItemStorageChannelTest.java
@@ -18,6 +18,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RefinedStorage2Test
 class ItemStorageChannelTest {
     @Test
+    void Test_setting_sources_should_clear_and_fill_list() {
+        // Arrange
+        ItemDiskStorage diskStorage1 = new ItemDiskStorage(10);
+        diskStorage1.insert(new ItemStack(Items.DIRT), 10, Action.EXECUTE);
+
+        ItemDiskStorage diskStorage2 = new ItemDiskStorage(10);
+        diskStorage2.insert(new ItemStack(Items.GLASS), 5, Action.EXECUTE);
+
+        ItemDiskStorage diskStorage3 = new ItemDiskStorage(10);
+        diskStorage3.insert(new ItemStack(Items.DIAMOND), 7, Action.EXECUTE);
+
+        ItemStorageChannel channel = new ItemStorageChannel();
+
+        // Act
+        Collection<ItemStack> list1 = new ArrayList<>(channel.getList().getAll());
+
+        channel.setSources(createSources(diskStorage3));
+        Collection<ItemStack> list2 = new ArrayList<>(channel.getList().getAll());
+
+        channel.setSources(createSources(diskStorage1, diskStorage2));
+        Collection<ItemStack> list3 = new ArrayList<>(channel.getList().getAll());
+
+        // Assert
+        assertItemStackListContents(list1.stream());
+        assertItemStackListContents(list2.stream(), new ItemStack(Items.DIAMOND, 7));
+        assertItemStackListContents(list3.stream(), new ItemStack(Items.DIRT, 10), new ItemStack(Items.GLASS, 5));
+    }
+
+    @Test
     void Test_inserting_without_any_storage_present() {
         // Arrange
         ItemStorageChannel storageChannel = new ItemStorageChannel();
@@ -148,6 +177,21 @@ class ItemStorageChannelTest {
         assertThat(result).isEmpty();
     }
 
+    @Test
+    void Test_extracting_without_item_present() {
+        // Arrange
+        ItemDiskStorage diskStorage = new ItemDiskStorage(10);
+        diskStorage.insert(new ItemStack(Items.DIRT), 10, Action.EXECUTE);
+        ItemStorageChannel storageChannel = new ItemStorageChannel();
+        storageChannel.setSources(createSources(diskStorage));
+
+        // Act
+        Optional<ItemStack> result = storageChannel.extract(new ItemStack(Items.GLASS), 10, Action.EXECUTE);
+
+        // Assert
+        assertThat(result).isEmpty();
+    }
+
     @ParameterizedTest
     @EnumSource(Action.class)
     void Test_single_source_partial_extract(Action action) {
@@ -223,33 +267,100 @@ class ItemStorageChannelTest {
         }
     }
 
-    @Test
-    void Test_setting_sources_should_clear_and_fill_list() {
+    @ParameterizedTest
+    @EnumSource(Action.class)
+    void Test_multiple_source_partial_extract(Action action) {
         // Arrange
         ItemDiskStorage diskStorage1 = new ItemDiskStorage(10);
         diskStorage1.insert(new ItemStack(Items.DIRT), 10, Action.EXECUTE);
 
-        ItemDiskStorage diskStorage2 = new ItemDiskStorage(10);
-        diskStorage2.insert(new ItemStack(Items.GLASS), 5, Action.EXECUTE);
 
-        ItemDiskStorage diskStorage3 = new ItemDiskStorage(10);
-        diskStorage3.insert(new ItemStack(Items.DIAMOND), 7, Action.EXECUTE);
+        ItemDiskStorage diskStorage2 = new ItemDiskStorage(5);
+        diskStorage2.insert(new ItemStack(Items.DIRT), 3, Action.EXECUTE);
 
-        ItemStorageChannel channel = new ItemStorageChannel();
+        ItemStorageChannel storageChannel = new ItemStorageChannel();
+        storageChannel.setSources(createSources(diskStorage1, diskStorage2));
 
         // Act
-        Collection<ItemStack> list1 = new ArrayList<>(channel.getList().getAll());
-
-        channel.setSources(createSources(diskStorage3));
-        Collection<ItemStack> list2 = new ArrayList<>(channel.getList().getAll());
-
-        channel.setSources(createSources(diskStorage1, diskStorage2));
-        Collection<ItemStack> list3 = new ArrayList<>(channel.getList().getAll());
+        Optional<ItemStack> result = storageChannel.extract(new ItemStack(Items.DIRT), 12, action);
 
         // Assert
-        assertItemStackListContents(list1.stream());
-        assertItemStackListContents(list2.stream(), new ItemStack(Items.DIAMOND, 7));
-        assertItemStackListContents(list3.stream(), new ItemStack(Items.DIRT, 10), new ItemStack(Items.GLASS, 5));
+        assertThat(result).isPresent();
+        assertItemStack(result.get(), new ItemStack(Items.DIRT, 12));
+
+        if (action == Action.EXECUTE) {
+            assertItemStackListContents(diskStorage1.getStacks());
+            assertItemStackListContents(diskStorage2.getStacks(), new ItemStack(Items.DIRT, 1));
+            assertItemStackListContents(storageChannel.getList(), new ItemStack(Items.DIRT, 1));
+        } else {
+            assertItemStackListContents(diskStorage1.getStacks(), new ItemStack(Items.DIRT, 10));
+            assertItemStackListContents(diskStorage2.getStacks(), new ItemStack(Items.DIRT, 3));
+            assertItemStackListContents(storageChannel.getList(), new ItemStack(Items.DIRT, 13));
+        }
+    }
+
+    @ParameterizedTest
+    @EnumSource(Action.class)
+    void Test_multiple_source_full_extract(Action action) {
+        // Arrange
+        ItemDiskStorage diskStorage1 = new ItemDiskStorage(10);
+        diskStorage1.insert(new ItemStack(Items.DIRT), 10, Action.EXECUTE);
+
+
+        ItemDiskStorage diskStorage2 = new ItemDiskStorage(5);
+        diskStorage2.insert(new ItemStack(Items.DIRT), 3, Action.EXECUTE);
+
+        ItemStorageChannel storageChannel = new ItemStorageChannel();
+        storageChannel.setSources(createSources(diskStorage1, diskStorage2));
+
+        // Act
+        Optional<ItemStack> result = storageChannel.extract(new ItemStack(Items.DIRT), 13, action);
+
+        // Assert
+        assertThat(result).isPresent();
+        assertItemStack(result.get(), new ItemStack(Items.DIRT, 13));
+
+        if (action == Action.EXECUTE) {
+            assertItemStackListContents(diskStorage1.getStacks());
+            assertItemStackListContents(diskStorage2.getStacks());
+            assertItemStackListContents(storageChannel.getList());
+        } else {
+            assertItemStackListContents(diskStorage1.getStacks(), new ItemStack(Items.DIRT, 10));
+            assertItemStackListContents(diskStorage2.getStacks(), new ItemStack(Items.DIRT, 3));
+            assertItemStackListContents(storageChannel.getList(), new ItemStack(Items.DIRT, 13));
+        }
+    }
+
+    @ParameterizedTest
+    @EnumSource(Action.class)
+    void Test_multiple_source_more_than_is_available_extract(Action action) {
+        // Arrange
+        ItemDiskStorage diskStorage1 = new ItemDiskStorage(10);
+        diskStorage1.insert(new ItemStack(Items.DIRT), 10, Action.EXECUTE);
+
+
+        ItemDiskStorage diskStorage2 = new ItemDiskStorage(5);
+        diskStorage2.insert(new ItemStack(Items.DIRT), 3, Action.EXECUTE);
+
+        ItemStorageChannel storageChannel = new ItemStorageChannel();
+        storageChannel.setSources(createSources(diskStorage1, diskStorage2));
+
+        // Act
+        Optional<ItemStack> result = storageChannel.extract(new ItemStack(Items.DIRT), 30, action);
+
+        // Assert
+        assertThat(result).isPresent();
+        assertItemStack(result.get(), new ItemStack(Items.DIRT, 13));
+
+        if (action == Action.EXECUTE) {
+            assertItemStackListContents(diskStorage1.getStacks());
+            assertItemStackListContents(diskStorage2.getStacks());
+            assertItemStackListContents(storageChannel.getList());
+        } else {
+            assertItemStackListContents(diskStorage1.getStacks(), new ItemStack(Items.DIRT, 10));
+            assertItemStackListContents(diskStorage2.getStacks(), new ItemStack(Items.DIRT, 3));
+            assertItemStackListContents(storageChannel.getList(), new ItemStack(Items.DIRT, 13));
+        }
     }
 
     private List<Storage<ItemStack>> createSources(Storage... storages) {
