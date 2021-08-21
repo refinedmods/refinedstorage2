d9eccd154e171c97d3608743d323e2cbdf9cd360
raoulvdberge <raoulvdberge@gmail.com>
2021-08-21 11:09:19 +0200

    Remove ThrottleHelper, replace with Guava RateLimiter

--- fabric/src/main/java/com/refinedmods/refinedstorage2/fabric/api/storage/disk/FabricRequestInfoCallback.java
+++ fabric/src/main/java/com/refinedmods/refinedstorage2/fabric/api/storage/disk/FabricRequestInfoCallback.java
@@ -1,26 +1,28 @@
 package com.refinedmods.refinedstorage2.fabric.api.storage.disk;
 
-import com.refinedmods.refinedstorage2.core.util.ThrottleHelper;
 import com.refinedmods.refinedstorage2.fabric.packet.PacketIds;
 import com.refinedmods.refinedstorage2.fabric.util.ClientPacketUtil;
 
 import java.util.UUID;
 import java.util.function.Consumer;
 
+import com.google.common.util.concurrent.RateLimiter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class FabricRequestInfoCallback implements Consumer<UUID> {
     private static final Logger LOGGER = LogManager.getLogger(FabricRequestInfoCallback.class);
 
-    private final ThrottleHelper<UUID> throttleHelper = new ThrottleHelper<>(500);
+    private final RateLimiter rateLimiter = RateLimiter.create(2);
 
     @Override
     public void accept(UUID id) {
-        throttleHelper.throttle(id, () -> {
-            LOGGER.debug("Sending request info packet for {}", id);
+        if (!rateLimiter.tryAcquire()) {
+            return;
+        }
 
-            ClientPacketUtil.sendToServer(PacketIds.STORAGE_DISK_INFO_REQUEST, data -> data.writeUuid(id));
-        }, System.currentTimeMillis());
+        LOGGER.debug("Sending request info packet for {}", id);
+
+        ClientPacketUtil.sendToServer(PacketIds.STORAGE_DISK_INFO_REQUEST, data -> data.writeUuid(id));
     }
 }
