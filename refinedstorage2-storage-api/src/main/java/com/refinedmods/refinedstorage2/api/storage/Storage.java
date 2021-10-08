5f1a487081feb56c826df4ad51d6d8328e05a438
raoulvdberge <raoulvdberge@gmail.com>
2021-10-08 09:46:28 +0200

    Introduce ExtractableStorage

--- refinedstorage2-storage-api/src/main/java/com/refinedmods/refinedstorage2/api/storage/Storage.java
+++ refinedstorage2-storage-api/src/main/java/com/refinedmods/refinedstorage2/api/storage/Storage.java
@@ -1,7 +1,4 @@
 package com.refinedmods.refinedstorage2.api.storage;
 
-import com.refinedmods.refinedstorage2.api.core.Action;
-
-public interface Storage<T> extends StorageView<T>, InsertableStorage<T> {
-    long extract(T resource, long amount, Action action);
+public interface Storage<T> extends StorageView<T>, InsertableStorage<T>, ExtractableStorage<T> {
 }
