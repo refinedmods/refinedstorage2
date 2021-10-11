4da1a8a9a63d09ef137a10c0c323321b6015ba5e
raoulvdberge <raoulvdberge@gmail.com>
2021-10-11 19:06:47 +0200

    New grid service: better insertion method

--- refinedstorage2-platform-fabric/src/main/java/com/refinedmods/refinedstorage2/platform/fabric/internal/grid/fluid/FluidGridEventHandlerImpl.java
+++ refinedstorage2-platform-fabric/src/main/java/com/refinedmods/refinedstorage2/platform/fabric/internal/grid/fluid/FluidGridEventHandlerImpl.java
@@ -1,18 +1,17 @@
 package com.refinedmods.refinedstorage2.platform.fabric.internal.grid.fluid;
 
+import com.refinedmods.refinedstorage2.api.core.Action;
 import com.refinedmods.refinedstorage2.api.grid.service.GridExtractMode;
 import com.refinedmods.refinedstorage2.api.grid.service.GridInsertMode;
 import com.refinedmods.refinedstorage2.api.grid.service.GridService;
 import com.refinedmods.refinedstorage2.platform.fabric.api.resource.FluidResource;
 
 import net.fabricmc.fabric.api.transfer.v1.context.ContainerItemContext;
-import net.fabricmc.fabric.api.transfer.v1.fluid.FluidConstants;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidStorage;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
 import net.fabricmc.fabric.api.transfer.v1.item.PlayerInventoryStorage;
 import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
 import net.fabricmc.fabric.api.transfer.v1.storage.StorageUtil;
-import net.fabricmc.fabric.api.transfer.v1.storage.base.ResourceAmount;
 import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
@@ -38,33 +37,21 @@ public class FluidGridEventHandlerImpl implements FluidGridEventHandler {
         if (cursorStorage == null) {
             return;
         }
-        try (Transaction tx = Transaction.openOuter()) {
-            ResourceAmount<FluidVariant> fluidVariantAmount = StorageUtil.findExtractableContent(cursorStorage, tx);
-            if (fluidVariantAmount == null) {
-                return;
-            }
-            long toExtract = getToExtract(insertMode, fluidVariantAmount);
-            long extracted = cursorStorage.extract(fluidVariantAmount.resource(), toExtract, tx);
-            if (extracted == 0) {
-                return;
-            }
-            // For fluid insertions, the entire stack needs to be insertable without remainder.
-            // We can't guarantee that every fluid storage accepts every remainder amount (buckets for example need at least 1 bucket).
-            boolean success = gridService.insert(new com.refinedmods.refinedstorage2.api.resource.ResourceAmount<>(
-                    new FluidResource(fluidVariantAmount.resource().getFluid(), fluidVariantAmount.resource().getNbt()),
-                    extracted
-            ));
-            if (success) {
-                tx.commit();
-            }
+        FluidVariant extractableResource = StorageUtil.findExtractableResource(cursorStorage, null);
+        if (extractableResource == null) {
+            return;
         }
-    }
-
-    private long getToExtract(GridInsertMode insertMode, ResourceAmount<FluidVariant> fluidVariantAmount) {
-        return switch (insertMode) {
-            case ENTIRE_RESOURCE -> fluidVariantAmount.amount();
-            case SINGLE_RESOURCE -> Math.min(fluidVariantAmount.amount(), FluidConstants.BUCKET);
-        };
+        FluidResource fluidResource = new FluidResource(extractableResource.getFluid(), extractableResource.getNbt());
+        gridService.insert(fluidResource, insertMode, (resource, amount, action) -> {
+            FluidVariant fluidVariant = resource.getFluidVariant();
+            try (Transaction tx = Transaction.openOuter()) {
+                long extracted = cursorStorage.extract(fluidVariant, amount, tx);
+                if (action == Action.EXECUTE) {
+                    tx.commit();
+                }
+                return extracted;
+            }
+        });
     }
 
     @Nullable
