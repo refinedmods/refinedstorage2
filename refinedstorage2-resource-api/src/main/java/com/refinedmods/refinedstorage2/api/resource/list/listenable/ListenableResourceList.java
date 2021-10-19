10ff6a9be705daeac703d2a50652937c3f0a5763
raoulvdberge <raoulvdberge@gmail.com>
2021-10-19 19:52:55 +0200

    Add API guardian and docs to resource API

--- refinedstorage2-resource-api/src/main/java/com/refinedmods/refinedstorage2/api/resource/list/listenable/ListenableResourceList.java
+++ refinedstorage2-resource-api/src/main/java/com/refinedmods/refinedstorage2/api/resource/list/listenable/ListenableResourceList.java
@@ -7,6 +7,17 @@ import com.refinedmods.refinedstorage2.api.resource.list.ResourceListOperationRe
 import java.util.Optional;
 import java.util.Set;
 
+import org.apiguardian.api.API;
+
+/**
+ * A resource list that can have listeners to track changes.
+ * Can easily be used with an existing list by passing it as a parent in the constructor.
+ * The {@link ResourceListListener#onChanged(ResourceListOperationResult)} method is only called when the change
+ * is being performed through this list, not the parent list.
+ *
+ * @param <T> the resource
+ */
+@API(status = API.Status.STABLE, since = "2.0.0-milestone.1.2")
 public class ListenableResourceList<T> extends ProxyResourceList<T> {
     private final Set<ResourceListListener<T>> listeners;
 
