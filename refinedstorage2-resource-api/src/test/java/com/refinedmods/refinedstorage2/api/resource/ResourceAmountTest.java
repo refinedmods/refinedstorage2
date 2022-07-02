cd05bd7260b1a403c8ec152858898f002723ee67
raoulvdberge <raoulvdberge@gmail.com>
2022-07-02 20:50:48 +0200

    Remove @Rs2Test in resource API tests

--- refinedstorage2-resource-api/src/test/java/com/refinedmods/refinedstorage2/api/resource/ResourceAmountTest.java
+++ refinedstorage2-resource-api/src/test/java/com/refinedmods/refinedstorage2/api/resource/ResourceAmountTest.java
@@ -1,32 +1,39 @@
 package com.refinedmods.refinedstorage2.api.resource;
 
-import com.refinedmods.refinedstorage2.test.Rs2Test;
-
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@Rs2Test
 class ResourceAmountTest {
     @Test
+    void testValidResource() {
+        // Act
+        final ResourceAmount<String> resourceAmount = new ResourceAmount<>("A", 1);
+
+        // Assert
+        assertThat(resourceAmount.getAmount()).isEqualTo(1);
+        assertThat(resourceAmount.getResource()).isEqualTo("A");
+    }
+
+    @Test
     @SuppressWarnings("ConstantConditions")
-    void Test_invalid_resource() {
+    void testInvalidResource() {
         // Act & assert
         assertThrows(NullPointerException.class, () -> new ResourceAmount<>(null, 1));
     }
 
     @Test
-    void Test_invalid_amount() {
+    void testInvalidAmount() {
         // Act & assert
         assertThrows(IllegalArgumentException.class, () -> new ResourceAmount<>("A", 0));
         assertThrows(IllegalArgumentException.class, () -> new ResourceAmount<>("A", -1));
     }
 
     @Test
-    void Test_invalid_increment_amount() {
+    void shouldNotIncrementZeroOrNegativeAmount() {
         // Arrange
-        ResourceAmount<String> sut = new ResourceAmount<>("A", 1);
+        final ResourceAmount<String> sut = new ResourceAmount<>("A", 1);
 
         // Act & assert
         assertThrows(IllegalArgumentException.class, () -> sut.increment(0));
@@ -34,20 +41,28 @@ class ResourceAmountTest {
     }
 
     @Test
-    void Test_invalid_decrement_amount() {
+    void shouldNotDecrementZeroOrNegativeAmount() {
         // Arrange
-        ResourceAmount<String> sut = new ResourceAmount<>("A", 3);
+        final ResourceAmount<String> sut = new ResourceAmount<>("A", 3);
 
         // Act & assert
-        assertThrows(IllegalArgumentException.class, () -> sut.decrement(3));
         assertThrows(IllegalArgumentException.class, () -> sut.decrement(0));
         assertThrows(IllegalArgumentException.class, () -> sut.decrement(-1));
     }
 
     @Test
-    void Test_incrementing() {
+    void shouldNotDecrementLeadingToZeroAmount() {
         // Arrange
-        ResourceAmount<String> sut = new ResourceAmount<>("A", 3);
+        final ResourceAmount<String> sut = new ResourceAmount<>("A", 3);
+
+        // Act & assert
+        assertThrows(IllegalArgumentException.class, () -> sut.decrement(3));
+    }
+
+    @Test
+    void shouldIncrement() {
+        // Arrange
+        final ResourceAmount<String> sut = new ResourceAmount<>("A", 3);
 
         // Act
         sut.increment(2);
@@ -57,24 +72,14 @@ class ResourceAmountTest {
     }
 
     @Test
-    void Test_decrementing() {
+    void shouldDecrement() {
         // Arrange
-        ResourceAmount<String> sut = new ResourceAmount<>("A", 3);
+        final ResourceAmount<String> sut = new ResourceAmount<>("A", 3);
 
         // Act
         sut.decrement(2);
 
         // Assert
         assertThat(sut.getAmount()).isEqualTo(1);
-    }
-
-    @Test
-    void Test_valid_resource_amount() {
-        // Act
-        ResourceAmount<String> resourceAmount = new ResourceAmount<>("A", 1);
-
-        // Assert
-        assertThat(resourceAmount.getAmount()).isEqualTo(1);
-        assertThat(resourceAmount.getResource()).isEqualTo("A");
     }
 }
