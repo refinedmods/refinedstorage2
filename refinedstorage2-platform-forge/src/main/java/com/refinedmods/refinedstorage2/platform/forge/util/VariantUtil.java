f1b0347188a2225c941dce1604a6fce99a864e64
raoulvdberge <raoulvdberge@gmail.com>
2022-07-30 21:01:20 +0200

    Fixed transferring items into Grid with NBT tag on Forge not working correctly.

--- refinedstorage2-platform-forge/src/main/java/com/refinedmods/refinedstorage2/platform/forge/util/VariantUtil.java
+++ refinedstorage2-platform-forge/src/main/java/com/refinedmods/refinedstorage2/platform/forge/util/VariantUtil.java
@@ -5,8 +5,12 @@ import com.refinedmods.refinedstorage2.platform.api.resource.ItemResource;
 
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public final class VariantUtil {
+    private static final Logger LOGGER = LogManager.getLogger();
+
     private VariantUtil() {
     }
 
@@ -15,6 +19,9 @@ public final class VariantUtil {
     }
 
     public static FluidStack toFluidStack(final FluidResource fluidResource, final long amount) {
+        if (amount > Integer.MAX_VALUE) {
+            LOGGER.warn("Truncating too large amount to fit into FluidStack {}", amount);
+        }
         return new FluidStack(fluidResource.fluid(), (int) amount, fluidResource.tag());
     }
 
@@ -23,6 +30,11 @@ public final class VariantUtil {
     }
 
     public static ItemStack toItemStack(final ItemResource itemResource, final long amount) {
-        return new ItemStack(itemResource.item(), (int) amount, itemResource.tag());
+        if (amount > Integer.MAX_VALUE) {
+            LOGGER.warn("Truncating too large amount to fit into ItemStack {}", amount);
+        }
+        final ItemStack stack = new ItemStack(itemResource.item(), (int) amount);
+        stack.setTag(itemResource.tag());
+        return stack;
     }
 }
