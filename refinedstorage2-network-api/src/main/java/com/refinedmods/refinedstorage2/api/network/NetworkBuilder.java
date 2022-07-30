faa77737f32ba5cafef09c43b843db5abfc93b4f
raoulvdberge <raoulvdberge@gmail.com>
2022-07-30 23:43:51 +0200

    Extract interface out of NetworkBuilder.

--- refinedstorage2-network-api/src/main/java/com/refinedmods/refinedstorage2/api/network/NetworkBuilder.java
+++ refinedstorage2-network-api/src/main/java/com/refinedmods/refinedstorage2/api/network/NetworkBuilder.java
@@ -1,176 +1,39 @@
 package com.refinedmods.refinedstorage2.api.network;
 
-import com.refinedmods.refinedstorage2.api.core.CoreValidations;
-import com.refinedmods.refinedstorage2.api.network.component.GraphNetworkComponent;
-import com.refinedmods.refinedstorage2.api.network.node.NetworkNode;
 import com.refinedmods.refinedstorage2.api.network.node.container.NetworkNodeContainer;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
-import javax.annotation.Nullable;
+import org.apiguardian.api.API;
 
-public class NetworkBuilder {
-    private final NetworkFactory networkFactory;
+@API(status = API.Status.STABLE, since = "2.0.0-milestone.1.0")
+public interface NetworkBuilder {
+    /**
+     * Initialized a not yet connected network node container.
+     * If the network node already has an associated {@link Network}, calling this will do nothing.
+     * This will perform a merge operation.
+     *
+     * @param container          the container
+     * @param connectionProvider the connection provider
+     * @return true if the container has no network yet, and the initialization succeeded, false otherwise
+     */
+    boolean initialize(NetworkNodeContainer container, ConnectionProvider connectionProvider);
 
-    public NetworkBuilder(final NetworkFactory networkFactory) {
-        this.networkFactory = networkFactory;
-    }
+    /**
+     * Removes a container from its network.
+     * Will remove the network if the container is the last container in the network, or otherwise it performs
+     * a split operation.
+     *
+     * @param container          the container
+     * @param connectionProvider the connection provider
+     */
+    void remove(NetworkNodeContainer container, ConnectionProvider connectionProvider);
 
-    public boolean initialize(final NetworkNodeContainer container, final ConnectionProvider connectionProvider) {
-        if (container.getNode().getNetwork() != null) {
-            return false;
-        }
-        final Connections connections = connectionProvider.findConnections(container, Collections.emptySet());
-        CoreValidations.validateEmpty(
-            connections.removedEntries(),
-            "Cannot have removed entries when starting from empty existing connections"
-        );
-        mergeNetworksOfNodes(connectionProvider, container, connections.foundEntries(), false);
-        return true;
-    }
-
-    private void mergeNetworksOfNodes(final ConnectionProvider connectionProvider,
-                                      final NetworkNodeContainer pivot,
-                                      final Set<NetworkNodeContainer> foundEntries,
-                                      final boolean pivotAlreadyHasNetwork) {
-        final Network pivotNetwork = pivotAlreadyHasNetwork
-            ? CoreValidations.validateNotNull(pivot.getNode().getNetwork(), "Pivot must have network")
-            : findPivotNetworkForMerge(connectionProvider, pivot, foundEntries).orElseGet(() -> createNetwork(pivot));
-
-        final Set<Network> mergedNetworks = new HashSet<>();
-
-        for (final NetworkNodeContainer entry : foundEntries) {
-            final NetworkNode entryNode = entry.getNode();
-            final boolean isNotInPivotNetwork = !pivotNetwork.getComponent(GraphNetworkComponent.class)
-                .getContainers().contains(entry);
-            if (isNotInPivotNetwork) {
-                final Network mergedNetwork = mergeNetworkOfNode(pivotNetwork, entry, entryNode);
-                if (mergedNetwork != null) {
-                    mergedNetworks.add(mergedNetwork);
-                }
-            }
-        }
-
-        mergedNetworks.forEach(mn -> mn.merge(pivotNetwork));
-    }
-
-    @Nullable
-    private Network mergeNetworkOfNode(final Network newNetwork,
-                                       final NetworkNodeContainer entry,
-                                       final NetworkNode entryNode) {
-        final Network oldNetwork = entryNode.getNetwork();
-        entryNode.setNetwork(newNetwork);
-        newNetwork.addContainer(entry);
-        return oldNetwork;
-    }
-
-    private Network createNetwork(final NetworkNodeContainer container) {
-        final Network network = networkFactory.create();
-        container.getNode().setNetwork(network);
-        network.addContainer(container);
-        return network;
-    }
-
-    private Optional<Network> findPivotNetworkForMerge(final ConnectionProvider connectionProvider,
-                                                       final NetworkNodeContainer addedContainer,
-                                                       final Set<NetworkNodeContainer> foundEntries) {
-        for (final NetworkNodeContainer entry : connectionProvider.sort(foundEntries)) {
-            if (entry == addedContainer) {
-                continue;
-            }
-            final Network entryNetwork = entry.getNode().getNetwork();
-            if (entryNetwork != null) {
-                return Optional.of(entryNetwork);
-            }
-        }
-        return Optional.empty();
-    }
-
-    public void remove(final NetworkNodeContainer container, final ConnectionProvider connectionProvider) {
-        final Network network = container.getNode().getNetwork();
-        if (network == null) {
-            throw new IllegalStateException("Cannot remove node that has no network yet");
-        }
-
-        final Set<NetworkNodeContainer> containers = network.getComponent(GraphNetworkComponent.class).getContainers();
-
-        final NetworkNodeContainer pivot = findPivotNodeForRemove(connectionProvider, container, containers);
-
-        if (pivot == null) {
-            network.remove();
-            container.getNode().setNetwork(null);
-            return;
-        }
-
-        final Connections connections = connectionProvider.findConnections(pivot, containers);
-        CoreValidations.validateContains(
-            connections.removedEntries(),
-            container,
-            "The removed container should be present in the removed entries, but isn't"
-        );
-        splitNetworks(connectionProvider, connections.removedEntries(), container);
-    }
-
-    public void update(final NetworkNodeContainer container, final ConnectionProvider connectionProvider) {
-        final Network network = container.getNode().getNetwork();
-        if (network == null) {
-            throw new IllegalStateException("Cannot update node that has no network yet");
-        }
-
-        final Set<NetworkNodeContainer> containers = network.getComponent(GraphNetworkComponent.class).getContainers();
-
-        final Connections connections = connectionProvider.findConnections(container, containers);
-        splitNetworks(connectionProvider, connections.removedEntries(), container);
-        mergeNetworksOfNodes(connectionProvider, container, connections.foundEntries(), true);
-    }
-
-    @Nullable
-    private NetworkNodeContainer findPivotNodeForRemove(final ConnectionProvider connectionProvider,
-                                                        final NetworkNodeContainer removedContainer,
-                                                        final Set<NetworkNodeContainer> containers) {
-        for (final NetworkNodeContainer entry : connectionProvider.sort(containers)) {
-            if (!entry.equals(removedContainer)) {
-                return entry;
-            }
-        }
-        return null;
-    }
-
-    private void splitNetworks(final ConnectionProvider connectionProvider,
-                               final Set<NetworkNodeContainer> removedEntries,
-                               final NetworkNodeContainer removedEntry) {
-        final Network networkOfRemovedNode = removedEntry.getNode().getNetwork();
-        if (networkOfRemovedNode == null) {
-            throw new IllegalStateException("Network of removed node cannot be empty");
-        }
-
-        removedEntries.forEach(e -> {
-            if (e.getNode().getNetwork() == null) {
-                throw new IllegalStateException("Network of resulting removed node cannot be empty");
-            }
-            e.getNode().getNetwork().removeContainer(e);
-            e.getNode().setNetwork(null);
-        });
-
-        final Set<Network> networksResultingOfSplit = removedEntries
-            .stream()
-            .filter(e -> !e.equals(removedEntry))
-            .map(e -> {
-                final boolean establishedNetwork = initialize(e, connectionProvider);
-                if (establishedNetwork) {
-                    return e.getNode().getNetwork();
-                }
-                return null;
-            })
-            .filter(Objects::nonNull)
-            .collect(Collectors.toSet());
-
-        if (!networksResultingOfSplit.isEmpty()) {
-            networkOfRemovedNode.split(networksResultingOfSplit);
-        }
-    }
+    /**
+     * Updates the network associated with the given container. Makes the network state adapt to connection changes
+     * of an already connected container.
+     * Will perform split and/or merge operations.
+     *
+     * @param container          the container
+     * @param connectionProvider the connection provider
+     */
+    void update(NetworkNodeContainer container, ConnectionProvider connectionProvider);
 }
