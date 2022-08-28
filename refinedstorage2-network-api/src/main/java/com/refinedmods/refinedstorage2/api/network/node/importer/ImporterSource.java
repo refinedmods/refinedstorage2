2d8d42324f40c0561bc04b80cd47470ae09bac2c
raoulvdberge <raoulvdberge@gmail.com>
2022-08-28 11:28:09 +0200

    Add docs for importer.

--- refinedstorage2-network-api/src/main/java/com/refinedmods/refinedstorage2/api/network/node/importer/ImporterSource.java
+++ refinedstorage2-network-api/src/main/java/com/refinedmods/refinedstorage2/api/network/node/importer/ImporterSource.java
@@ -5,6 +5,21 @@ import com.refinedmods.refinedstorage2.api.storage.InsertableStorage;
 
 import java.util.Iterator;
 
+import org.apiguardian.api.API;
+
+/**
+ * Represents a source for the importer.
+ * A valid source for the importer needs to be an {@link ExtractableStorage}, so the resources can be extracted
+ * for insertion into the target network.
+ * A valid source for the importer needs to be a {@link InsertableStorage} as well, so that transfers that end up
+ * failing can be given back to the source.
+ *
+ * @param <T> the resource type
+ */
+@API(status = API.Status.STABLE, since = "2.0.0-milestone.2.1")
 public interface ImporterSource<T> extends ExtractableStorage<T>, InsertableStorage<T> {
+    /**
+     * @return the resources that this source has
+     */
     Iterator<T> getResources();
 }
--- refinedstorage2-network-api/src/main/java/com/refinedmods/refinedstorage2/api/network/node/importer/ImporterTransferStrategy.java
+++ refinedstorage2-network-api/src/main/java/com/refinedmods/refinedstorage2/api/network/node/importer/ImporterTransferStrategy.java
@@ -4,6 +4,12 @@ import com.refinedmods.refinedstorage2.api.core.filter.Filter;
 import com.refinedmods.refinedstorage2.api.network.Network;
 import com.refinedmods.refinedstorage2.api.storage.Actor;
 
+import org.apiguardian.api.API;
+
+/**
+ * An importer transfer strategy transfers resources from a source to the network.
+ */
+@API(status = API.Status.STABLE, since = "2.0.0-milestone.2.1")
 public interface ImporterTransferStrategy {
     boolean transfer(Filter filter, Actor actor, Network network);
 }
