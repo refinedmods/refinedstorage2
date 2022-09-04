1e51e46b2d1fbfbcdf1284f7dd8127c465eb786a
raoulvdberge <raoulvdberge@gmail.com>
2022-09-04 20:33:55 +0200

    Enfore package-info.java existence, but not for tests.

---
+++ refinedstorage2-platform-forge/src/main/java/com/refinedmods/refinedstorage2/platform/forge/containermenu/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@FieldsAndMethodsAreNonnullByDefault
+package com.refinedmods.refinedstorage2.platform.forge.containermenu;
+
+import com.refinedmods.refinedstorage2.api.core.FieldsAndMethodsAreNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
