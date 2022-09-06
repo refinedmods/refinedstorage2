6f05df480e1854446dc851128a925da9eab92e72
raoulvdberge <raoulvdberge@gmail.com>
2022-09-06 20:36:25 +0200

    Platform exporter strategy factory.

---
+++ refinedstorage2-platform-fabric/src/main/java/com/refinedmods/refinedstorage2/platform/fabric/internal/network/node/exporter/StorageExporterTransferStrategyFactory.java
@@ -0,0 +1,55 @@
+package com.refinedmods.refinedstorage2.platform.fabric.internal.network.node.exporter;
+
+import com.refinedmods.refinedstorage2.api.network.node.exporter.AbstractExporterTransferStrategy;
+import com.refinedmods.refinedstorage2.api.network.node.exporter.ExporterTransferStrategy;
+import com.refinedmods.refinedstorage2.api.storage.channel.StorageChannelType;
+import com.refinedmods.refinedstorage2.platform.api.network.node.exporter.ExporterTransferStrategyFactory;
+import com.refinedmods.refinedstorage2.platform.fabric.internal.network.node.StorageInsertableStorage;
+
+import java.util.Optional;
+import java.util.function.Function;
+import javax.annotation.Nullable;
+
+import net.fabricmc.fabric.api.lookup.v1.block.BlockApiLookup;
+import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+
+public class StorageExporterTransferStrategyFactory<T, P> implements ExporterTransferStrategyFactory {
+    private final BlockApiLookup<Storage<P>, Direction> lookup;
+    private final StorageChannelType<T> storageChannelType;
+    private final Function<T, P> toPlatformMapper;
+    private final Function<Object, Optional<T>> mapper;
+    private final long singleAmount;
+
+    public StorageExporterTransferStrategyFactory(final BlockApiLookup<Storage<P>, Direction> lookup,
+                                                  final StorageChannelType<T> storageChannelType,
+                                                  final Function<Object, Optional<T>> mapper,
+                                                  final Function<T, P> toPlatformMapper,
+                                                  final long singleAmount) {
+        this.lookup = lookup;
+        this.storageChannelType = storageChannelType;
+        this.mapper = mapper;
+        this.toPlatformMapper = toPlatformMapper;
+        this.singleAmount = singleAmount;
+    }
+
+    @Override
+    public ExporterTransferStrategy create(final ServerLevel level,
+                                           final BlockPos pos,
+                                           final Direction direction,
+                                           final boolean hasStackUpgrade) {
+        return new AbstractExporterTransferStrategy<>(
+            new StorageInsertableStorage<>(lookup, toPlatformMapper, level, pos, direction),
+            storageChannelType,
+            hasStackUpgrade ? singleAmount * 64 : singleAmount
+        ) {
+            @Nullable
+            @Override
+            protected T tryConvert(final Object resource) {
+                return mapper.apply(resource).orElse(null);
+            }
+        };
+    }
+}
