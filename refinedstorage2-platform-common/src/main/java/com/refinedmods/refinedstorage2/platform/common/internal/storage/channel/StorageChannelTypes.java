5b14fe53da51dad3ef228599d2796ca9237d232b
raoulvdberge <raoulvdberge@gmail.com>
2022-09-07 20:08:11 +0200

    Implement a fuzzy resource list.
    This is so that the exporter can check what fuzzy resources to export.

--- refinedstorage2-platform-common/src/main/java/com/refinedmods/refinedstorage2/platform/common/internal/storage/channel/StorageChannelTypes.java
+++ refinedstorage2-platform-common/src/main/java/com/refinedmods/refinedstorage2/platform/common/internal/storage/channel/StorageChannelTypes.java
@@ -1,15 +1,25 @@
 package com.refinedmods.refinedstorage2.platform.common.internal.storage.channel;
 
+import com.refinedmods.refinedstorage2.api.resource.list.ResourceList;
+import com.refinedmods.refinedstorage2.api.resource.list.ResourceListImpl;
 import com.refinedmods.refinedstorage2.api.storage.channel.StorageChannelImpl;
 import com.refinedmods.refinedstorage2.api.storage.channel.StorageChannelType;
 import com.refinedmods.refinedstorage2.platform.api.resource.FluidResource;
 import com.refinedmods.refinedstorage2.platform.api.resource.ItemResource;
+import com.refinedmods.refinedstorage2.platform.api.resource.list.FuzzyResourceListImpl;
 
 public final class StorageChannelTypes {
-    public static final StorageChannelType<ItemResource> ITEM =
-        new NamedStorageChannelType<>("ITEM", StorageChannelImpl::new);
-    public static final StorageChannelType<FluidResource> FLUID =
-        new NamedStorageChannelType<>("FLUID", StorageChannelImpl::new);
+    public static final StorageChannelType<ItemResource> ITEM = new NamedStorageChannelType<>("ITEM", () -> {
+        final ResourceList<ItemResource> list = new ResourceListImpl<>();
+        final ResourceList<ItemResource> fuzzyList = new FuzzyResourceListImpl<>(list);
+        return new StorageChannelImpl<>(fuzzyList);
+    });
+
+    public static final StorageChannelType<FluidResource> FLUID = new NamedStorageChannelType<>("FLUID", () -> {
+        final ResourceList<FluidResource> list = new ResourceListImpl<>();
+        final ResourceList<FluidResource> fuzzyList = new FuzzyResourceListImpl<>(list);
+        return new StorageChannelImpl<>(fuzzyList);
+    });
 
     private StorageChannelTypes() {
     }
