a1519845cfd786f7118139e4db5f23e9d42cee34
raoulvdberge <raoulvdberge@gmail.com>
2022-09-25 18:02:09 +0200

    Mutation coverage improvements to core API

--- refinedstorage2-core-api/src/test/java/com/refinedmods/refinedstorage2/api/core/QuantityFormatterTest.java
+++ refinedstorage2-core-api/src/test/java/com/refinedmods/refinedstorage2/api/core/QuantityFormatterTest.java
@@ -15,13 +15,14 @@ class QuantityFormatterTest {
     }
 
     @Test
-    void shouldFormatWithUnitsForLargerNumber() {
+    void shouldFormatWithUnitsForThousands() {
         // Act & assert
         assertThat(QuantityFormatter.formatWithUnits(1000)).isEqualTo("1K");
         assertThat(QuantityFormatter.formatWithUnits(1510)).isEqualTo("1.5K");
 
         assertThat(QuantityFormatter.formatWithUnits(10_000)).isEqualTo("10K");
         assertThat(QuantityFormatter.formatWithUnits(10_510)).isEqualTo("10.5K");
+        assertThat(QuantityFormatter.formatWithUnits(99_999)).isEqualTo("99.9K");
 
         assertThat(QuantityFormatter.formatWithUnits(100_000)).isEqualTo("100K");
         assertThat(QuantityFormatter.formatWithUnits(100_500)).isEqualTo("100K");
@@ -40,6 +41,7 @@ class QuantityFormatterTest {
 
         assertThat(QuantityFormatter.formatWithUnits(10_000_000)).isEqualTo("10M");
         assertThat(QuantityFormatter.formatWithUnits(10_510_000)).isEqualTo("10.5M");
+        assertThat(QuantityFormatter.formatWithUnits(99_999_999)).isEqualTo("99.9M");
 
         assertThat(QuantityFormatter.formatWithUnits(100_000_000)).isEqualTo("100M");
         assertThat(QuantityFormatter.formatWithUnits(100_510_000)).isEqualTo("100M");
@@ -55,6 +57,7 @@ class QuantityFormatterTest {
         // Act & assert
         assertThat(QuantityFormatter.formatWithUnits(1_000_000_000)).isEqualTo("1B");
         assertThat(QuantityFormatter.formatWithUnits(1_010_000_000)).isEqualTo("1B");
+        assertThat(QuantityFormatter.formatWithUnits(1_100_000_000)).isEqualTo("1.1B");
         assertThat(QuantityFormatter.formatWithUnits(1_100_001_000)).isEqualTo("1.1B");
         assertThat(QuantityFormatter.formatWithUnits(1_920_001_000)).isEqualTo("1.9B");
     }
