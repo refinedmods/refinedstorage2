bb3d42b97b9850ee28fa97b8cdde3a7953c9a502
raoulvdberge <raoulvdberge@gmail.com>
2022-10-01 17:17:26 +0200

    Add upgrade destination for the exporter.

--- refinedstorage2-platform-common/src/main/java/com/refinedmods/refinedstorage2/platform/common/internal/upgrade/UpgradeDestinations.java
+++ refinedstorage2-platform-common/src/main/java/com/refinedmods/refinedstorage2/platform/common/internal/upgrade/UpgradeDestinations.java
@@ -1,13 +1,15 @@
 package com.refinedmods.refinedstorage2.platform.common.internal.upgrade;
 
 import com.refinedmods.refinedstorage2.platform.api.upgrade.UpgradeDestination;
+import com.refinedmods.refinedstorage2.platform.common.content.ContentIds;
 
 import net.minecraft.network.chat.Component;
 
 import static com.refinedmods.refinedstorage2.platform.common.util.IdentifierUtil.createTranslation;
 
 public enum UpgradeDestinations implements UpgradeDestination {
-    IMPORTER(createTranslation("block", "importer"));
+    IMPORTER(createTranslation("block", ContentIds.IMPORTER.getPath())),
+    EXPORTER(createTranslation("block", ContentIds.EXPORTER.getPath()));
 
     private final Component name;
 
