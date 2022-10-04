95237746b3dd71a97de20af57af4d05dd62cdc0c
raoulvdberge <raoulvdberge@gmail.com>
2022-10-04 20:32:59 +0200

    Remove some duplications in the block entity code.

---
+++ refinedstorage2-platform-common/src/main/java/com/refinedmods/refinedstorage2/platform/common/block/entity/AbstractLevelInteractingUpgradeableNetworkNodeContainerBlockEntity.java
@@ -0,0 +1,133 @@
+package com.refinedmods.refinedstorage2.platform.common.block.entity;
+
+import com.refinedmods.refinedstorage2.api.network.node.AbstractNetworkNode;
+import com.refinedmods.refinedstorage2.platform.api.PlatformApi;
+import com.refinedmods.refinedstorage2.platform.common.Platform;
+import com.refinedmods.refinedstorage2.platform.common.content.Items;
+import com.refinedmods.refinedstorage2.platform.common.internal.upgrade.UpgradeDestinations;
+
+import java.util.Set;
+
+import com.google.common.util.concurrent.RateLimiter;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public abstract class AbstractLevelInteractingUpgradeableNetworkNodeContainerBlockEntity<T extends AbstractNetworkNode>
+    extends AbstractInternalNetworkNodeContainerBlockEntity<T> {
+    private static final Logger LOGGER = LogManager.getLogger();
+    private static final String TAG_UPGRADES = "u";
+
+    protected final UpgradeContainer upgradeContainer;
+    private RateLimiter rateLimiter = createRateLimiter(0);
+
+    protected AbstractLevelInteractingUpgradeableNetworkNodeContainerBlockEntity(
+        final BlockEntityType<?> type,
+        final BlockPos pos,
+        final BlockState state,
+        final T node,
+        final UpgradeDestinations destination
+    ) {
+        super(type, pos, state, node);
+        this.upgradeContainer = new UpgradeContainer(
+            destination,
+            PlatformApi.INSTANCE.getUpgradeRegistry(),
+            this::upgradeContainerChanged
+        );
+    }
+
+    // used to handle rotations
+    @Override
+    @SuppressWarnings("deprecation")
+    public void setBlockState(final BlockState newBlockState) {
+        super.setBlockState(newBlockState);
+        if (!(level instanceof ServerLevel serverLevel)) {
+            return;
+        }
+        initialize(serverLevel);
+    }
+
+    @Override
+    public void setLevel(final Level level) {
+        super.setLevel(level);
+        if (!(level instanceof ServerLevel serverLevel)) {
+            return;
+        }
+        initialize(serverLevel);
+    }
+
+    protected final void initialize(final ServerLevel level) {
+        final Direction direction = getDirection();
+        if (direction == null) {
+            LOGGER.warn(
+                "Failed to initialize: could not extract direction from block at {}, state is {}",
+                worldPosition,
+                getBlockState()
+            );
+            return;
+        }
+        initialize(level, direction);
+    }
+
+    protected abstract void initialize(ServerLevel level, Direction direction);
+
+    @Override
+    public void doWork() {
+        if (rateLimiter.tryAcquire()) {
+            super.doWork();
+        }
+    }
+
+    private void upgradeContainerChanged() {
+        configureAccordingToUpgrades();
+        setChanged();
+        if (level instanceof ServerLevel serverLevel) {
+            initialize(serverLevel);
+        }
+    }
+
+    @Override
+    public void saveAdditional(final CompoundTag tag) {
+        super.saveAdditional(tag);
+        tag.put(TAG_UPGRADES, upgradeContainer.createTag());
+    }
+
+    @Override
+    public void load(final CompoundTag tag) {
+        if (tag.contains(TAG_UPGRADES)) {
+            upgradeContainer.fromTag(tag.getList(TAG_UPGRADES, Tag.TAG_COMPOUND));
+        }
+        configureAccordingToUpgrades();
+        super.load(tag);
+    }
+
+    private void configureAccordingToUpgrades() {
+        final int amountOfSpeedUpgrades = upgradeContainer.countItem(Items.INSTANCE.getSpeedUpgrade());
+        final boolean hasStackUpgrade = hasStackUpgrade();
+        final long upgradeEnergyUsage = calculateUpgradeEnergyUsage(amountOfSpeedUpgrades, hasStackUpgrade);
+        this.rateLimiter = createRateLimiter(amountOfSpeedUpgrades);
+        this.setEnergyUsage(upgradeEnergyUsage);
+    }
+
+    protected final boolean hasStackUpgrade() {
+        return upgradeContainer.hasAnyOf(Set.of(Items.INSTANCE.getStackUpgrade()));
+    }
+
+    private long calculateUpgradeEnergyUsage(final long amountOfSpeedUpgrades, final boolean hasStackUpgrade) {
+        return (Platform.INSTANCE.getConfig().getUpgrade().getSpeedUpgradeEnergyUsage() * amountOfSpeedUpgrades)
+            + (hasStackUpgrade ? Platform.INSTANCE.getConfig().getUpgrade().getStackUpgradeEnergyUsage() : 0L);
+    }
+
+    protected abstract void setEnergyUsage(long upgradeEnergyUsage);
+
+    private static RateLimiter createRateLimiter(final int amountOfSpeedUpgrades) {
+        return RateLimiter.create((double) amountOfSpeedUpgrades + 1);
+    }
+}
--- refinedstorage2-platform-common/src/main/java/com/refinedmods/refinedstorage2/platform/common/block/entity/ImporterBlockEntity.java
+++ refinedstorage2-platform-common/src/main/java/com/refinedmods/refinedstorage2/platform/common/block/entity/ImporterBlockEntity.java
@@ -10,20 +10,16 @@ import com.refinedmods.refinedstorage2.platform.api.resource.FuzzyModeNormalizer
 import com.refinedmods.refinedstorage2.platform.common.Platform;
 import com.refinedmods.refinedstorage2.platform.common.containermenu.ImporterContainerMenu;
 import com.refinedmods.refinedstorage2.platform.common.content.BlockEntities;
-import com.refinedmods.refinedstorage2.platform.common.content.Items;
 import com.refinedmods.refinedstorage2.platform.common.internal.resource.filter.ResourceFilterContainer;
 import com.refinedmods.refinedstorage2.platform.common.internal.upgrade.UpgradeDestinations;
 import com.refinedmods.refinedstorage2.platform.common.menu.ExtendedMenuProvider;
 
 import java.util.List;
-import java.util.Set;
 import javax.annotation.Nullable;
 
-import com.google.common.util.concurrent.RateLimiter;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.Tag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
@@ -31,33 +27,31 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import static com.refinedmods.refinedstorage2.platform.common.util.IdentifierUtil.createTranslation;
 
-public class ImporterBlockEntity extends AbstractInternalNetworkNodeContainerBlockEntity<ImporterNetworkNode>
+public class ImporterBlockEntity
+    extends AbstractLevelInteractingUpgradeableNetworkNodeContainerBlockEntity<ImporterNetworkNode>
     implements ExtendedMenuProvider {
     private static final Logger LOGGER = LogManager.getLogger();
 
     private static final String TAG_FILTER_MODE = "fim";
     private static final String TAG_EXACT_MODE = "em";
     private static final String TAG_RESOURCE_FILTER = "rf";
-    private static final String TAG_UPGRADES = "u";
 
     private final ResourceFilterContainer resourceFilterContainer;
-    private final UpgradeContainer upgradeContainer;
     private boolean exactMode;
-    private RateLimiter rateLimiter = createRateLimiter(0);
 
     public ImporterBlockEntity(final BlockPos pos, final BlockState state) {
         super(
             BlockEntities.INSTANCE.getImporter(),
             pos,
             state,
-            new ImporterNetworkNode(calculateEnergyUsage(0, false))
+            new ImporterNetworkNode(0),
+            UpgradeDestinations.IMPORTER
         );
         getNode().setNormalizer(value -> FuzzyModeNormalizer.tryNormalize(exactMode, value));
         this.resourceFilterContainer = new ResourceFilterContainer(
@@ -65,57 +59,16 @@ public class ImporterBlockEntity extends AbstractInternalNetworkNodeContainerBlo
             9,
             this::resourceFilterContainerChanged
         );
-        this.upgradeContainer = new UpgradeContainer(
-            UpgradeDestinations.IMPORTER,
-            PlatformApi.INSTANCE.getUpgradeRegistry(),
-            this::upgradeContainerChanged
-        );
-    }
-
-    // used to handle rotations
-    @Override
-    @SuppressWarnings("deprecation")
-    public void setBlockState(final BlockState newBlockState) {
-        super.setBlockState(newBlockState);
-        if (!(level instanceof ServerLevel serverLevel)) {
-            return;
-        }
-        updateTransferStrategy(serverLevel);
     }
 
     @Override
-    public void setLevel(final Level level) {
-        super.setLevel(level);
-        if (!(level instanceof ServerLevel serverLevel)) {
-            return;
-        }
-        updateTransferStrategy(serverLevel);
-    }
-
-    @Override
-    public void doWork() {
-        if (rateLimiter.tryAcquire()) {
-            super.doWork();
-        }
-    }
-
-    private void updateTransferStrategy(final ServerLevel serverLevel) {
-        final Direction direction = getDirection();
-        if (direction == null) {
-            LOGGER.warn(
-                "Could not extract direction from importer block at {}, state is {}",
-                worldPosition,
-                getBlockState()
-            );
-            return;
-        }
-        final CompositeImporterTransferStrategy strategy = createStrategy(serverLevel, direction);
+    protected void initialize(final ServerLevel level, final Direction direction) {
+        final CompositeImporterTransferStrategy strategy = createStrategy(level, direction);
         LOGGER.info("Initialized importer at {} with strategy {}", worldPosition, strategy);
         getNode().setTransferStrategy(strategy);
     }
 
-    private CompositeImporterTransferStrategy createStrategy(final ServerLevel serverLevel,
-                                                             final Direction direction) {
+    private CompositeImporterTransferStrategy createStrategy(final ServerLevel serverLevel, final Direction direction) {
         final boolean hasStackUpgrade = hasStackUpgrade();
         final Direction incomingDirection = direction.getOpposite();
         final BlockPos sourcePosition = worldPosition.offset(direction.getNormal());
@@ -132,7 +85,6 @@ public class ImporterBlockEntity extends AbstractInternalNetworkNodeContainerBlo
     public void saveAdditional(final CompoundTag tag) {
         super.saveAdditional(tag);
         tag.put(TAG_RESOURCE_FILTER, resourceFilterContainer.toTag());
-        tag.put(TAG_UPGRADES, upgradeContainer.createTag());
         tag.putInt(TAG_FILTER_MODE, FilterModeSettings.getFilterMode(getNode().getFilterMode()));
         tag.putBoolean(TAG_EXACT_MODE, exactMode);
     }
@@ -150,13 +102,7 @@ public class ImporterBlockEntity extends AbstractInternalNetworkNodeContainerBlo
         if (tag.contains(TAG_RESOURCE_FILTER)) {
             resourceFilterContainer.load(tag.getCompound(TAG_RESOURCE_FILTER));
         }
-
-        if (tag.contains(TAG_UPGRADES)) {
-            upgradeContainer.fromTag(tag.getList(TAG_UPGRADES, Tag.TAG_COMPOUND));
-        }
-
         initializeResourceFilter();
-        initializeUpgrades();
 
         super.load(tag);
     }
@@ -189,29 +135,10 @@ public class ImporterBlockEntity extends AbstractInternalNetworkNodeContainerBlo
         getNode().setFilterTemplates(resourceFilterContainer.getUniqueTemplates());
     }
 
-    private void upgradeContainerChanged() {
-        initializeUpgrades();
-        setChanged();
-        if (level instanceof ServerLevel serverLevel) {
-            updateTransferStrategy(serverLevel);
-        }
-    }
-
-    private void initializeUpgrades() {
-        final int amountOfSpeedUpgrades = upgradeContainer.countItem(Items.INSTANCE.getSpeedUpgrade());
-        final boolean hasStackUpgrade = hasStackUpgrade();
-        rateLimiter = createRateLimiter(amountOfSpeedUpgrades);
-        getNode().setEnergyUsage(calculateEnergyUsage(amountOfSpeedUpgrades, hasStackUpgrade));
-    }
-
-    private boolean hasStackUpgrade() {
-        return upgradeContainer.hasAnyOf(Set.of(Items.INSTANCE.getStackUpgrade()));
-    }
-
-    private static long calculateEnergyUsage(final int amountOfSpeedUpgrades, final boolean hasStackUpgrade) {
-        return Platform.INSTANCE.getConfig().getImporter().getEnergyUsage()
-            + (Platform.INSTANCE.getConfig().getUpgrade().getSpeedUpgradeEnergyUsage() * amountOfSpeedUpgrades)
-            + (hasStackUpgrade ? Platform.INSTANCE.getConfig().getUpgrade().getStackUpgradeEnergyUsage() : 0L);
+    @Override
+    protected void setEnergyUsage(final long upgradeEnergyUsage) {
+        final long baseEnergyUsage = Platform.INSTANCE.getConfig().getImporter().getEnergyUsage();
+        getNode().setEnergyUsage(baseEnergyUsage + upgradeEnergyUsage);
     }
 
     @Override
