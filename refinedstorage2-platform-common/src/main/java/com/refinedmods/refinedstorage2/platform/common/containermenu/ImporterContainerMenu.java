f4e8e726bf4baa70ee3c086a9ca43c29946724c6
raoulvdberge <raoulvdberge@gmail.com>
2022-10-04 21:10:27 +0200

    Remove some duplications in the container code.

--- refinedstorage2-platform-common/src/main/java/com/refinedmods/refinedstorage2/platform/common/containermenu/ImporterContainerMenu.java
+++ refinedstorage2-platform-common/src/main/java/com/refinedmods/refinedstorage2/platform/common/containermenu/ImporterContainerMenu.java
@@ -7,7 +7,6 @@ import com.refinedmods.refinedstorage2.platform.common.block.entity.UpgradeConta
 import com.refinedmods.refinedstorage2.platform.common.containermenu.property.ClientProperty;
 import com.refinedmods.refinedstorage2.platform.common.containermenu.property.PropertyTypes;
 import com.refinedmods.refinedstorage2.platform.common.containermenu.property.ServerProperty;
-import com.refinedmods.refinedstorage2.platform.common.containermenu.slot.ResourceFilterSlot;
 import com.refinedmods.refinedstorage2.platform.common.content.Menus;
 import com.refinedmods.refinedstorage2.platform.common.internal.resource.filter.ResourceFilterContainer;
 import com.refinedmods.refinedstorage2.platform.common.internal.upgrade.UpgradeDestinations;
@@ -16,26 +15,18 @@ import com.refinedmods.refinedstorage2.platform.common.util.RedstoneMode;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.Slot;
 
-public class ImporterContainerMenu extends AbstractResourceFilterContainerMenu implements ResourceTypeAccessor {
-    private static final int FILTER_SLOT_X = 8;
-    private static final int FILTER_SLOT_Y = 20;
-
+public class ImporterContainerMenu extends AbstractSimpleFilterContainerMenu<ImporterBlockEntity>
+    implements ResourceTypeAccessor {
     public ImporterContainerMenu(final int syncId, final Inventory playerInventory, final FriendlyByteBuf buf) {
-        super(Menus.INSTANCE.getImporter(), syncId, PlatformApi.INSTANCE.getResourceTypeRegistry());
-
-        addSlots(
+        super(
+            Menus.INSTANCE.getImporter(),
+            syncId,
+            PlatformApi.INSTANCE.getResourceTypeRegistry(),
             playerInventory.player,
-            new ResourceFilterContainer(PlatformApi.INSTANCE.getResourceTypeRegistry(), 9),
-            new UpgradeContainer(UpgradeDestinations.IMPORTER, PlatformApi.INSTANCE.getUpgradeRegistry())
+            buf,
+            UpgradeDestinations.IMPORTER
         );
-
-        initializeResourceFilterSlots(buf);
-
-        registerProperty(new ClientProperty<>(PropertyTypes.FILTER_MODE, FilterMode.BLOCK));
-        registerProperty(new ClientProperty<>(PropertyTypes.EXACT_MODE, false));
-        registerProperty(new ClientProperty<>(PropertyTypes.REDSTONE_MODE, RedstoneMode.IGNORE));
     }
 
     public ImporterContainerMenu(final int syncId,
@@ -48,44 +39,35 @@ public class ImporterContainerMenu extends AbstractResourceFilterContainerMenu i
             syncId,
             PlatformApi.INSTANCE.getResourceTypeRegistry(),
             player,
-            resourceFilterContainer
+            resourceFilterContainer,
+            upgradeContainer,
+            importer
         );
-        addSlots(player, resourceFilterContainer, upgradeContainer);
+    }
 
+    @Override
+    protected void registerClientProperties() {
+        registerProperty(new ClientProperty<>(PropertyTypes.FILTER_MODE, FilterMode.BLOCK));
+        registerProperty(new ClientProperty<>(PropertyTypes.EXACT_MODE, false));
+        registerProperty(new ClientProperty<>(PropertyTypes.REDSTONE_MODE, RedstoneMode.IGNORE));
+    }
+
+    @Override
+    protected void registerServerProperties(final ImporterBlockEntity blockEntity) {
         registerProperty(new ServerProperty<>(
             PropertyTypes.FILTER_MODE,
-            importer::getFilterMode,
-            importer::setFilterMode
+            blockEntity::getFilterMode,
+            blockEntity::setFilterMode
         ));
         registerProperty(new ServerProperty<>(
             PropertyTypes.EXACT_MODE,
-            importer::isExactMode,
-            importer::setExactMode
+            blockEntity::isExactMode,
+            blockEntity::setExactMode
         ));
         registerProperty(new ServerProperty<>(
             PropertyTypes.REDSTONE_MODE,
-            importer::getRedstoneMode,
-            importer::setRedstoneMode
+            blockEntity::getRedstoneMode,
+            blockEntity::setRedstoneMode
         ));
-    }
-
-    private void addSlots(final Player player,
-                          final ResourceFilterContainer resourceFilterContainer,
-                          final UpgradeContainer upgradeContainer) {
-        for (int i = 0; i < resourceFilterContainer.size(); ++i) {
-            addSlot(createFilterSlot(resourceFilterContainer, i));
-        }
-        for (int i = 0; i < upgradeContainer.getContainerSize(); ++i) {
-            addSlot(new Slot(upgradeContainer, i, 187, 6 + (i * 18)));
-        }
-        addPlayerInventory(player.getInventory(), 8, 55);
-
-        transferManager.addBiTransfer(player.getInventory(), upgradeContainer);
-        transferManager.addFilterTransfer(player.getInventory());
-    }
-
-    private Slot createFilterSlot(final ResourceFilterContainer resourceFilterContainer, final int i) {
-        final int x = FILTER_SLOT_X + (18 * i);
-        return new ResourceFilterSlot(resourceFilterContainer, i, x, FILTER_SLOT_Y);
     }
 }
