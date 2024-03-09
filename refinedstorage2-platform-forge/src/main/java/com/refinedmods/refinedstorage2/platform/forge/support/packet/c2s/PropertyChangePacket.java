23f9170d28df867ba998544107c87fc1507f15eb
raoulvdberge <raoulvdberge@gmail.com>
2024-03-09 18:48:28 +0100

    fix: changing side buttons doesn't work on Forge

--- refinedstorage2-platform-forge/src/main/java/com/refinedmods/refinedstorage2/platform/forge/support/packet/c2s/PropertyChangePacket.java
+++ refinedstorage2-platform-forge/src/main/java/com/refinedmods/refinedstorage2/platform/forge/support/packet/c2s/PropertyChangePacket.java
@@ -1,13 +1,14 @@
 package com.refinedmods.refinedstorage2.platform.forge.support.packet.c2s;
 
 import com.refinedmods.refinedstorage2.platform.common.support.AbstractBaseContainerMenu;
+import com.refinedmods.refinedstorage2.platform.common.support.packet.PacketIds;
 
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.neoforge.network.handling.PlayPayloadContext;
 
-public record PropertyChangePacket(ResourceLocation id, int value) implements CustomPacketPayload {
+public record PropertyChangePacket(ResourceLocation propertyId, int value) implements CustomPacketPayload {
     public static PropertyChangePacket decode(final FriendlyByteBuf buf) {
         return new PropertyChangePacket(buf.readResourceLocation(), buf.readInt());
     }
@@ -15,14 +16,19 @@ public record PropertyChangePacket(ResourceLocation id, int value) implements Cu
     public static void handle(final PropertyChangePacket packet, final PlayPayloadContext ctx) {
         ctx.player().ifPresent(player -> ctx.workHandler().submitAsync(() -> {
             if (player.containerMenu instanceof AbstractBaseContainerMenu menu) {
-                menu.receivePropertyChangeFromClient(packet.id, packet.value);
+                menu.receivePropertyChangeFromClient(packet.propertyId, packet.value);
             }
         }));
     }
 
     @Override
     public void write(final FriendlyByteBuf buf) {
-        buf.writeResourceLocation(id);
+        buf.writeResourceLocation(propertyId);
         buf.writeInt(value);
+    }
+
+    @Override
+    public ResourceLocation id() {
+        return PacketIds.PROPERTY_CHANGE;
     }
 }
