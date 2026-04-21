refinedmods/refinedstorage2

commit b292460e958c0341dbe7e2d173a5e16402297d6d

feat: ability to disable energy usage
Fixes GH-1274

refinedstorage-common/src/main/java/com/refinedmods/refinedstorage/common/support/network/item/NetworkItemContextImpl.java
@@ -67,10 +67,14 @@ class NetworkItemContextImpl implements NetworkItemContext {
 
     @Override
     public boolean isActive() {
-        return slotReference.resolve(player)
-            .flatMap(Platform.INSTANCE::getEnergyStorage)
-            .map(energyStorage -> energyStorage.getStored() > 0)
-            .orElse(true);
+        return slotReference.resolve(player).map(stack -> {
+            if (!Platform.INSTANCE.getConfig().isRequireEnergy()) {
+                return true;
+            }
+            return Platform.INSTANCE.getEnergyStorage(stack)
+                .map(energyStorage -> energyStorage.getStored() > 0)
+                .orElse(false);
+        }).orElse(false);
     }
 
     @Override

refinedstorage-common/src/main/java/com/refinedmods/refinedstorage/common/Config.java
@@ -18,6 +18,8 @@ public interface Config {
 
     void setScreenSize(ScreenSize size);
 
+    boolean isRequireEnergy();
+
     boolean isDebug();
 
     boolean isSmoothScrolling();

refinedstorage-common/src/main/java/com/refinedmods/refinedstorage/common/autocrafting/monitor/WirelessAutocraftingMonitor.java
@@ -35,9 +35,10 @@ class WirelessAutocraftingMonitor implements AutocraftingMonitor {
 
     @Override
     public boolean isAutocraftingMonitorActive() {
-        final boolean networkActive = context.resolveNetwork().map(
-            network -> network.getComponent(EnergyNetworkComponent.class).getStored() > 0
-        ).orElse(false);
+        final boolean networkActive = context.resolveNetwork()
+            .map(network -> !Platform.INSTANCE.getConfig().isRequireEnergy()
+                || network.getComponent(EnergyNetworkComponent.class).getStored() > 0)
+            .orElse(false);
         return networkActive && context.isActive();
     }
 

refinedstorage-common/src/main/java/com/refinedmods/refinedstorage/common/content/CreativeModeTabItems.java
@@ -1,5 +1,6 @@
 package com.refinedmods.refinedstorage.common.content;
 
+import com.refinedmods.refinedstorage.common.Platform;
 import com.refinedmods.refinedstorage.common.misc.ProcessorItem;
 import com.refinedmods.refinedstorage.common.storage.FluidStorageVariant;
 import com.refinedmods.refinedstorage.common.storage.ItemStorageVariant;
@@ -20,10 +21,13 @@ public final class CreativeModeTabItems {
     }
 
     private static void appendBlocks(final Consumer<ItemStack> consumer) {
+        final boolean requireEnergy = Platform.INSTANCE.getConfig().isRequireEnergy();
         final Consumer<ItemLike> itemConsumer = item -> consumer.accept(new ItemStack(item));
         appendDefaultBlockColor(consumer, Blocks.INSTANCE.getController());
-        consumer.accept(Items.INSTANCE.getControllers().getFirst().get().createAtEnergyCapacity());
-        appendDefaultBlockColor(consumer, Blocks.INSTANCE.getCreativeController());
+        if (requireEnergy) {
+            consumer.accept(Items.INSTANCE.getControllers().getFirst().get().createAtEnergyCapacity());
+            appendDefaultBlockColor(consumer, Blocks.INSTANCE.getCreativeController());
+        }
         appendDefaultBlockColor(consumer, Blocks.INSTANCE.getCable());
         appendDefaultBlockColor(consumer, Blocks.INSTANCE.getImporter());
         appendDefaultBlockColor(consumer, Blocks.INSTANCE.getExporter());
@@ -36,8 +40,10 @@ public final class CreativeModeTabItems {
         appendDefaultBlockColor(consumer, Blocks.INSTANCE.getCraftingGrid());
         appendDefaultBlockColor(consumer, Blocks.INSTANCE.getPatternGrid());
         itemConsumer.accept(Items.INSTANCE.getPortableGrid());
-        consumer.accept(Items.INSTANCE.getPortableGrid().createAtEnergyCapacity());
-        itemConsumer.accept(Items.INSTANCE.getCreativePortableGrid());
+        if (requireEnergy) {
+            consumer.accept(Items.INSTANCE.getPortableGrid().createAtEnergyCapacity());
+            itemConsumer.accept(Items.INSTANCE.getCreativePortableGrid());
+        }
         appendDefaultBlockColor(consumer, Blocks.INSTANCE.getDetector());
         itemConsumer.accept(Blocks.INSTANCE.getInterface());
         Arrays.stream(ItemStorageVariant.values()).forEach(variant -> itemConsumer.accept(
@@ -64,11 +70,13 @@ public final class CreativeModeTabItems {
 
     public static void appendColoredVariants(final Consumer<ItemStack> consumer) {
         appendColoredBlocks(consumer, Blocks.INSTANCE.getController());
-        final var controllers = Items.INSTANCE.getControllers();
-        for (int i = 1; i < controllers.size(); ++i) {
-            consumer.accept(controllers.get(i).get().createAtEnergyCapacity());
+        if (Platform.INSTANCE.getConfig().isRequireEnergy()) {
+            final var controllers = Items.INSTANCE.getControllers();
+            for (int i = 1; i < controllers.size(); ++i) {
+                consumer.accept(controllers.get(i).get().createAtEnergyCapacity());
+            }
+            appendColoredBlocks(consumer, Blocks.INSTANCE.getCreativeController());
         }
-        appendColoredBlocks(consumer, Blocks.INSTANCE.getCreativeController());
         appendColoredBlocks(consumer, Blocks.INSTANCE.getCable());
         appendColoredBlocks(consumer, Blocks.INSTANCE.getImporter());
         appendColoredBlocks(consumer, Blocks.INSTANCE.getExporter());
@@ -99,7 +107,9 @@ public final class CreativeModeTabItems {
     }
 
     private static void appendItems(final Consumer<ItemStack> consumer) {
+        final boolean requireEnergy = Platform.INSTANCE.getConfig().isRequireEnergy();
         final Consumer<ItemLike> itemConsumer = item -> consumer.accept(new ItemStack(item));
+
         itemConsumer.accept(Items.INSTANCE.getQuartzEnrichedIron());
         itemConsumer.accept(Items.INSTANCE.getQuartzEnrichedCopper());
         itemConsumer.accept(Items.INSTANCE.getSilicon());
@@ -140,15 +150,19 @@ public final class CreativeModeTabItems {
         itemConsumer.accept(Items.INSTANCE.getCreativeRangeUpgrade());
         itemConsumer.accept(Items.INSTANCE.getAutocraftingUpgrade());
         itemConsumer.accept(Items.INSTANCE.getWirelessGrid());
-        consumer.accept(Items.INSTANCE.getWirelessGrid().createAtEnergyCapacity());
-        itemConsumer.accept(Items.INSTANCE.getCreativeWirelessGrid());
+        if (requireEnergy) {
+            consumer.accept(Items.INSTANCE.getWirelessGrid().createAtEnergyCapacity());
+            itemConsumer.accept(Items.INSTANCE.getCreativeWirelessGrid());
+        }
         itemConsumer.accept(Items.INSTANCE.getConfigurationCard());
         itemConsumer.accept(Items.INSTANCE.getNetworkCard());
         itemConsumer.accept(Items.INSTANCE.getSecurityCard());
         itemConsumer.accept(Items.INSTANCE.getFallbackSecurityCard());
         itemConsumer.accept(Items.INSTANCE.getPattern());
         itemConsumer.accept(Items.INSTANCE.getWirelessAutocraftingMonitor());
-        consumer.accept(Items.INSTANCE.getWirelessAutocraftingMonitor().createAtEnergyCapacity());
-        itemConsumer.accept(Items.INSTANCE.getCreativeWirelessAutocraftingMonitor());
+        if (requireEnergy) {
+            consumer.accept(Items.INSTANCE.getWirelessAutocraftingMonitor().createAtEnergyCapacity());
+            itemConsumer.accept(Items.INSTANCE.getCreativeWirelessAutocraftingMonitor());
+        }
     }
 }

refinedstorage-common/src/main/java/com/refinedmods/refinedstorage/common/controller/ControllerBlockItem.java
@@ -9,15 +9,21 @@ import com.refinedmods.refinedstorage.common.api.support.energy.AbstractEnergyBl
 import com.refinedmods.refinedstorage.common.content.BlockEntities;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.Style;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.component.TooltipDisplay;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -25,6 +31,10 @@ import net.minecraft.world.level.block.state.BlockState;
 import static com.refinedmods.refinedstorage.common.util.IdentifierUtil.createTranslation;
 
 public class ControllerBlockItem extends AbstractEnergyBlockItem {
+    private static final MutableComponent NOT_NEEDED =
+        createTranslation("item", "controller.not_needed_because_energy_is_not_required")
+            .withStyle(ChatFormatting.RED);
+
     private final Block block;
 
     ControllerBlockItem(final Identifier id, final Block block) {
@@ -39,6 +49,15 @@ public class ControllerBlockItem extends AbstractEnergyBlockItem {
     @Override
     public Component getName(final ItemStack stack) {
         return block.getName();
+    }
+
+    @Override
+    public void appendHoverText(final ItemStack stack, final TooltipContext context, final TooltipDisplay display,
+                                final Consumer<Component> builder, final TooltipFlag flag) {
+        super.appendHoverText(stack, context, display, builder, flag);
+        if (!Platform.INSTANCE.getConfig().isRequireEnergy()) {
+            builder.accept(NOT_NEEDED);
+        }
     }
 
     @Override

refinedstorage-common/src/main/java/com/refinedmods/refinedstorage/common/grid/WirelessGrid.java
@@ -82,7 +82,8 @@ class WirelessGrid implements Grid {
     @Override
     public boolean isGridActive() {
         final boolean networkActive = context.resolveNetwork()
-            .map(network -> network.getComponent(EnergyNetworkComponent.class).getStored() > 0)
+            .map(network -> !Platform.INSTANCE.getConfig().isRequireEnergy()
+                || network.getComponent(EnergyNetworkComponent.class).getStored() > 0)
             .orElse(false);
         return networkActive && context.isActive();
     }

refinedstorage-common/src/main/java/com/refinedmods/refinedstorage/common/networking/RelayScreen.java
@@ -1,5 +1,6 @@
 package com.refinedmods.refinedstorage.common.networking;
 
+import com.refinedmods.refinedstorage.common.Platform;
 import com.refinedmods.refinedstorage.common.storage.AccessModeSideButtonWidget;
 import com.refinedmods.refinedstorage.common.storage.FilterModeSideButtonWidget;
 import com.refinedmods.refinedstorage.common.storage.StoragePropertyTypes;
@@ -63,9 +64,12 @@ public class RelayScreen extends AbstractFilterScreen<RelayContainerMenu>
     }
 
     private void addPassButtons(final boolean visible) {
-        passEnergyButton = new RelayPassEnergySideButtonWidget(getMenu().getProperty(RelayPropertyTypes.PASS_ENERGY));
-        passEnergyButton.visible = visible;
-        addSideButton(passEnergyButton);
+        if (Platform.INSTANCE.getConfig().isRequireEnergy()) {
+            passEnergyButton = new RelayPassEnergySideButtonWidget(
+                getMenu().getProperty(RelayPropertyTypes.PASS_ENERGY));
+            passEnergyButton.visible = visible;
+            addSideButton(passEnergyButton);
+        }
 
         passSecurityButton = new RelayPassSecuritySideButtonWidget(
             getMenu().getProperty(RelayPropertyTypes.PASS_SECURITY)

refinedstorage-common/src/main/java/com/refinedmods/refinedstorage/common/storage/portablegrid/PortableGrid.java
@@ -105,7 +105,7 @@ class PortableGrid implements Grid {
 
     @Override
     public boolean isGridActive() {
-        return energyStorage.getStored() > 0 && storage != null;
+        return storage != null && (energyStorage.getStored() > 0 || !Platform.INSTANCE.getConfig().isRequireEnergy());
     }
 
     @Override

commit d822c49a9e9e59f13575cfd659aeae0b594685f5

fix: creative variants of energy items not being active
Fixes GH-1295

Regression introduced in b292460e

refinedstorage-common/src/main/java/com/refinedmods/refinedstorage/common/support/network/item/NetworkItemContextImpl.java
@@ -73,7 +73,7 @@ class NetworkItemContextImpl implements NetworkItemContext {
             }
             return Platform.INSTANCE.getEnergyStorage(stack)
                 .map(energyStorage -> energyStorage.getStored() > 0)
-                .orElse(false);
+                .orElse(true);
         }).orElse(false);
     }
 