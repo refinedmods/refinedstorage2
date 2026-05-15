refinedmods/refinedstorage2

commit 10d84098b2f5464432235bbff1ecc3abf15aca94

fix: grid search only matching original item names and not renamed items
Fixes GH-1259

refinedstorage-common-api/src/main/java/com/refinedmods/refinedstorage/common/api/grid/view/AbstractGridResource.java
@@ -14,15 +14,24 @@ import org.jspecify.annotations.Nullable;
 @API(status = API.Status.STABLE, since = "2.0.0-milestone.3.0")
 public abstract class AbstractGridResource<T extends PlatformResourceKey> implements GridResource {
     protected final T resource;
-    private final String name;
+    private final String sortName;
+    private final Set<String> searchableNames;
     private final Function<GridResourceAttributeKey, Set<String>> attributes;
 
     protected AbstractGridResource(final T resource,
-                                   final String name,
+                                   final String sortName,
+                                   final Set<String> searchableNames,
                                    final Function<GridResourceAttributeKey, Set<String>> attributes) {
         this.resource = resource;
-        this.name = name;
+        this.sortName = sortName;
+        this.searchableNames = searchableNames;
         this.attributes = attributes;
+    }
+
+    protected AbstractGridResource(final T resource,
+                                   final String sortName,
+                                   final Function<GridResourceAttributeKey, Set<String>> attributes) {
+        this(resource, sortName, Set.of(sortName), attributes);
     }
 
     @Override
@@ -39,8 +48,13 @@ public abstract class AbstractGridResource<T extends PlatformResourceKey> implem
     }
 
     @Override
-    public String getName() {
-        return name;
+    public String getSortName() {
+        return sortName;
+    }
+
+    @Override
+    public Set<String> getSearchableNames() {
+        return searchableNames;
     }
 
     @Override
@@ -63,7 +77,7 @@ public abstract class AbstractGridResource<T extends PlatformResourceKey> implem
     public String toString() {
         return "AbstractGridResource{"
             + "resource=" + resource
-            + ", name='" + name + '\''
+            + ", names='" + searchableNames + '\''
             + ", attributes=" + attributes
             + '}';
     }

refinedstorage-common-api/src/main/java/com/refinedmods/refinedstorage/common/api/grid/view/GridResource.java
@@ -31,7 +31,9 @@ public interface GridResource {
 
     long getAmount(ResourceRepository<GridResource> repository);
 
-    String getName();
+    String getSortName();
+
+    Set<String> getSearchableNames();
 
     Set<String> getAttribute(GridResourceAttributeKey key);
 

refinedstorage-common/src/main/java/com/refinedmods/refinedstorage/common/grid/GridSortingTypes.java
@@ -11,7 +11,7 @@ import org.jspecify.annotations.Nullable;
 
 public enum GridSortingTypes {
     QUANTITY(trp -> view -> Comparator.comparingLong(value -> value.getAmount(view))),
-    NAME(trp -> view -> Comparator.comparing(GridResource::getName)),
+    NAME(trp -> view -> Comparator.comparing(GridResource::getSortName)),
     ID(trp -> view -> Comparator.comparingInt(GridResource::getRegistryId)),
     LAST_MODIFIED(trp -> view -> (a, b) -> {
         final long lastModifiedA = extractTime(trp.getTrackedResource(a));

refinedstorage-common/src/main/java/com/refinedmods/refinedstorage/common/grid/query/GridQueryParser.java
@@ -178,7 +178,14 @@ public class GridQueryParser {
     }
 
     private static ResourceRepositoryFilter<GridResource> parseLiteral(final LiteralNode node) {
-        return (repository, resource) -> normalize(resource.getName()).contains(normalize(node.token().content()));
+        return (repository, resource) -> {
+            for (final String name : resource.getSearchableNames()) {
+                if (normalize(name).contains(normalize(node.token().content()))) {
+                    return true;
+                }
+            }
+            return false;
+        };
     }
 
     private static ResourceRepositoryFilter<GridResource> and(

refinedstorage-common/src/main/java/com/refinedmods/refinedstorage/common/grid/view/AbstractItemGridResourceRepositoryMapper.java
@@ -35,7 +35,8 @@ public abstract class AbstractItemGridResourceRepositoryMapper implements Resour
         final ItemResource itemResource = (ItemResource) resource;
         final Item item = itemResource.item();
         final ItemStack itemStack = itemResource.toItemStack();
-        final String name = item.getName(itemStack).getString();
+        final String originalName = item.getName(itemStack).getString();
+        final String customName = itemStack.getHoverName().getString();
         final String modId = getModId(itemStack);
         final String modName = getModName(modId).orElse("");
         final Map<GridResourceAttributeKey, Supplier<Set<String>>> attributes = Map.of(
@@ -47,7 +48,8 @@ public abstract class AbstractItemGridResourceRepositoryMapper implements Resour
         return new ItemGridResource(
             itemResource,
             itemStack,
-            name,
+            customName,
+            originalName,
             k -> attributes.getOrDefault(k, Collections::emptySet).get()
         );
     }

refinedstorage-common/src/main/java/com/refinedmods/refinedstorage/common/grid/view/ItemGridResource.java
@@ -15,6 +15,7 @@ import com.refinedmods.refinedstorage.common.support.resource.ResourceTypes;
 import com.refinedmods.refinedstorage.common.support.tooltip.MouseClientTooltipComponent;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -45,12 +46,20 @@ public class ItemGridResource extends AbstractGridResource<ItemResource> {
 
     public ItemGridResource(final ItemResource resource,
                             final ItemStack itemStack,
-                            final String name,
+                            final String customName,
+                            final String originalName,
                             final Function<GridResourceAttributeKey, Set<String>> attributes) {
-        super(resource, name, attributes);
+        super(resource, customName, createSearchableNames(customName, originalName), attributes);
         this.id = Item.getId(resource.item());
         this.itemStack = itemStack;
         this.itemResource = resource;
+    }
+
+    private static Set<String> createSearchableNames(final String customName, final String originalName) {
+        final Set<String> names = new HashSet<>();
+        names.add(customName);
+        names.add(originalName);
+        return names;
     }
 
     public ItemStack getItemStack() {

refinedstorage-common/src/test/java/com/refinedmods/refinedstorage/common/grid/GridSortingTypesTest.java
@@ -51,7 +51,7 @@ class GridSortingTypesTest {
     void setUp() {
         builder = new ResourceRepositoryBuilderImpl<>(
             MAPPER,
-            view -> Comparator.comparing(GridResource::getName),
+            view -> Comparator.comparing(GridResource::getSortName),
             view -> Comparator.comparingLong(resource -> resource.getAmount(view))
         );
         dirt = new ItemResource(Items.DIRT, DataComponentPatch.EMPTY);
@@ -85,28 +85,28 @@ class GridSortingTypesTest {
         // Assert
         switch (sortingType) {
             case QUANTITY -> assertThat(repository.getViewList())
-                .extracting(GridResource::getName)
+                .extracting(GridResource::getSortName)
                 .containsExactly(
                     "Stone",
                     "Gold Ingot",
                     "Dirt"
                 );
             case NAME -> assertThat(repository.getViewList())
-                .extracting(GridResource::getName)
+                .extracting(GridResource::getSortName)
                 .containsExactly(
                     "Dirt",
                     "Gold Ingot",
                     "Stone"
                 );
             case ID -> assertThat(repository.getViewList())
-                .extracting(GridResource::getName)
+                .extracting(GridResource::getSortName)
                 .containsExactly(
                     "Stone",
                     "Dirt",
                     "Gold Ingot"
                 );
             case LAST_MODIFIED -> assertThat(repository.getViewList())
-                .extracting(GridResource::getName)
+                .extracting(GridResource::getSortName)
                 .containsExactly(
                     "Gold Ingot",
                     "Stone",
@@ -142,28 +142,28 @@ class GridSortingTypesTest {
         // Assert
         switch (sortingType) {
             case QUANTITY -> assertThat(repository.getViewList())
-                .extracting(GridResource::getName)
+                .extracting(GridResource::getSortName)
                 .containsExactly(
                     "Dirt",
                     "Gold Ingot",
                     "Stone"
                 );
             case NAME -> assertThat(repository.getViewList())
-                .extracting(GridResource::getName)
+                .extracting(GridResource::getSortName)
                 .containsExactly(
                     "Stone",
                     "Gold Ingot",
                     "Dirt"
                 );
             case ID -> assertThat(repository.getViewList())
-                .extracting(GridResource::getName)
+                .extracting(GridResource::getSortName)
                 .containsExactly(
                     "Gold Ingot",
                     "Dirt",
                     "Stone"
                 );
             case LAST_MODIFIED -> assertThat(repository.getViewList())
-                .extracting(GridResource::getName)
+                .extracting(GridResource::getSortName)
                 .containsExactly(
                     "Dirt",
                     "Stone",

refinedstorage-common/src/test/java/com/refinedmods/refinedstorage/common/grid/query/GridQueryParserImplTest.java
@@ -52,7 +52,7 @@ class GridQueryParserImplTest {
         },
         MutableResourceListImpl.create(),
         new HashSet<>(),
-        v -> Comparator.comparing(GridResource::getName),
+        v -> Comparator.comparing(GridResource::getSortName),
         v -> Comparator.comparingLong(resource -> resource.getAmount(v))
     );
 
@@ -69,13 +69,24 @@ class GridQueryParserImplTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"dirt", "Dirt", "DiRt", "Di", "irt"})
-    void testNameQuery(final String query) throws GridQueryParserException {
+    void testSortNameQuery(final String query) throws GridQueryParserException {
         // Act
         final var predicate = sut.parse(query);
 
         // Assert
         assertThat(predicate.test(repository, new R("Dirt"))).isTrue();
         assertThat(predicate.test(repository, new R("Glass"))).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"dirt", "Dirt", "DiRt", "Di", "irt", "Grass", "grass", "Gr"})
+    void testSearchableNamesQuery(final String query) throws GridQueryParserException {
+        // Act
+        final var predicate = sut.parse(query);
+
+        // Assert
+        assertThat(predicate.test(repository, new R("Doesn'tMatter", Set.of("Dirt", "Grass")))).isTrue();
+        assertThat(predicate.test(repository, new R("Glass", Set.of("Glass", "Clear")))).isFalse();
     }
 
     @ParameterizedTest
@@ -287,28 +298,39 @@ class GridQueryParserImplTest {
     }
 
     private static class R implements GridResource {
-        private final String name;
+        private final String sortName;
+        private final Set<String> searchableNames;
         private final long amount;
         private final Map<GridResourceAttributeKey, Set<String>> attributes;
 
-        R(final String name) {
-            this(name, 1);
+        R(final String sortName) {
+            this(sortName, 1);
         }
 
-        R(final String name, final long amount) {
-            this.name = name;
+        R(final String sortName, final Set<String> searchableNames) {
+            this(sortName, searchableNames, 1);
+        }
+
+        R(final String sortName, final long amount) {
+            this(sortName, Set.of(sortName), amount);
+        }
+
+        R(final String sortName, final Set<String> searchableNames, final long amount) {
+            this.sortName = sortName;
+            this.searchableNames = searchableNames;
             this.amount = amount;
             this.attributes = Map.of();
         }
 
         R(
-            final String name,
+            final String sortName,
             final long amount,
             final String modId,
             final String modName,
             final Set<String> tags
         ) {
-            this.name = name;
+            this.sortName = sortName;
+            this.searchableNames = Set.of(sortName);
             this.amount = amount;
             this.attributes = Map.of(
                 GridResourceAttributeKeys.MOD_ID, Set.of(modId),
@@ -331,8 +353,13 @@ class GridQueryParserImplTest {
         }
 
         @Override
-        public String getName() {
-            return name;
+        public String getSortName() {
+            return sortName;
+        }
+
+        @Override
+        public Set<String> getSearchableNames() {
+            return searchableNames;
         }
 
         @Override

commit 3af72398f17e3933ea9fb6f478747c2bff69177e

fix: inconsistent tostring
Introduced in 10d84098

refinedstorage-common-api/src/main/java/com/refinedmods/refinedstorage/common/api/grid/view/AbstractGridResource.java
@@ -77,7 +77,8 @@ public abstract class AbstractGridResource<T extends PlatformResourceKey> implem
     public String toString() {
         return "AbstractGridResource{"
             + "resource=" + resource
-            + ", names='" + searchableNames + '\''
+            + ", sortName='" + sortName + '\''
+            + ", searchableNames=" + searchableNames
             + ", attributes=" + attributes
             + '}';
     }