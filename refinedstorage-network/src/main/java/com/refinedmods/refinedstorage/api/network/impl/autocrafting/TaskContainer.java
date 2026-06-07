cba482903cf34447b534b48d5bc502d978b2fae9
raoulvdberge <raoulvdberge@gmail.com>
2026-06-07 17:17:33 +0200

    style: checkstyle issue

--- refinedstorage-network/src/main/java/com/refinedmods/refinedstorage/api/network/impl/autocrafting/TaskContainer.java
+++ refinedstorage-network/src/main/java/com/refinedmods/refinedstorage/api/network/impl/autocrafting/TaskContainer.java
@@ -19,7 +19,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
-import javax.annotation.Nullable;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
